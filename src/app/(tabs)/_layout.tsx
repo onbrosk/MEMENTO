55c182b5c035dd79e8dc9d1d4ee34cfaaebdbb0d
Kristián Kurimský <kr.kurimsky@gmail.com>
2026-09-20 19:09:40 +0200

    changed styling of nav

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -2,9 +2,21 @@
 import { Tabs } from 'expo-router';
 import { useTheme } from '../../hooks/useTheme';
 export default function TabLayout() {
-  const theme = useTheme
+  const theme = useTheme()
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+    <Tabs screenOptions={{
+      tabBarActiveTintColor: theme.primary,
+      headerTintColor: theme.text,
+      headerStyle: { backgroundColor: theme.background },
+      tabBarStyle: {
+        backgroundColor: theme.backgroundSecondary,
+        borderTopWidth: 0,
+        elevation: 0,
+        shadowColor: 'transparent',
+      },
+      tabBarInactiveTintColor: theme.textMuted
+    }}>
+
       <Tabs.Screen
         name="index"
         options={{
