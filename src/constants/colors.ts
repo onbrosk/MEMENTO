f919d740047c381abdd87ec8d682f8fe019d02f1
Kristián Kurimský <kr.kurimsky@gmail.com>
2026-09-20 18:09:50 +0200

    inplemented collor pallete system with device default option

---
+++ src/constants/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  dark: {
+    background: "#1A1C1F",
+    text: "#FFFFFF",
+    textMuted: "#D9D9D9",
+    primary: "#A9C8E8",
+  },
+  light: {
+    background: "#FFFFFF",
+    text: "#000000",
+    textMuted: "#4D4D4D",
+    primary: "#007AFF",
+  },
+};
