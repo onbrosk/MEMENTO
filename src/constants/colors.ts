df89735e019ce1671846d920c41a148ecf451269
Kristián Kurimský <kr.kurimsky@gmail.com>
2026-09-20 19:07:51 +0200

    added backgroundSecondary color

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,12 +1,14 @@
 export const colors = {
   dark: {
     background: "#1A1C1F",
+    backgorundSecondary: "#2B3137",
     text: "#FFFFFF",
     textMuted: "#D9D9D9",
     primary: "#A9C8E8",
   },
   light: {
     background: "#FFFFFF",
+    backgroundSecondary: "#d2d0d0",
     text: "#000000",
     textMuted: "#4D4D4D",
     primary: "#007AFF",
